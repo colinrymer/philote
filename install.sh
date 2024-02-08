953f54ae5872108fb6ded5cc37027978a88cb2c1
Colin Rymer <colin.rymer@gmail.com>
2024-02-08 13:35:20 -0500

    feat(install.sh): support homebrew install on apple silicon

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ set -u
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+if test -f /opt/homebrew/bin/brew; then 
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 brew install ansible
 
 cd $TMPDIR
