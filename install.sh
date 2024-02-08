f96c781ef8a9bf1f7b08602cd9088c9cc4be30df
Colin Rymer <colin.rymer@gmail.com>
2024-02-08 13:42:15 -0500

    feat(install.sh): better error handling

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -u
+  #!/bin/bash
+set -eux
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
@@ -7,9 +7,17 @@ if test -f /opt/homebrew/bin/brew; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-brew install ansible
+brew list | grep ansible
+
+if [ -z $(brew list | grep ansible) ]; then
+  brew install ansible
+fi
 
 cd $TMPDIR
+
+if test -f philote.zip; then
+  rm -f philote.zip
+fi
 
 curl -fsSL https://github.com/colinrymer/philote/archive/master.zip -o philote.zip
 unzip philote.zip
