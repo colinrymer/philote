c0c3232d54e98dab815f0b75fd719e5183134526
Colin Rymer <colin.rymer@gmail.com>
2024-03-11 08:51:36 +0700

    chore(install.sh): better handling of temp dir

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
   #!/bin/bash
 set -eux
 
-if ! command -v brew &> /dev/null; then
+# Try to add homebrew to PATH on apple silicon
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if ! command -v brew &> /dev/null ; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
@@ -13,10 +16,15 @@ if [ -z $(brew list | grep ansible) ]; then
   brew install ansible
 fi
 
-cd $TMPDIR
+clean_up() {
+  test -d "$tmp_dir" && rm -rf "$tmp_dir"
+}
 
-rm -f philote.zip
-rm -rf philote-master
+tmp_dir=$(mktemp -d -t philote)
+trap "clean_up ${tmp_dir}" EXIT
+echo "Using directory: ${tmp_dir}"
+
+cd $tmp_dir
 
 curl -fsSL https://github.com/colinrymer/philote/archive/master.zip -o philote.zip
 unzip philote.zip
